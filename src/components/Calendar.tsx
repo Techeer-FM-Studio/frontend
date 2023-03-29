603624ff2d5836e286f53b788e57a0fe2f4d7b66
TMInstaller <ajvldmlqjqclr@naver.com>
2023-03-29 12:18:20 +0900

    feat: 전체적인 달력 UI 완성

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,4 +1,84 @@
 import styles from '../styles/Calendar.module.scss';
-export default function Calendar() {
-  return <div className={styles.calendar}>Calendar</div>;
-}
+import { useState } from 'react';
+import moment from 'moment';
+
+const Calendar = () => {
+  const [getMoment, setMoment] = useState(moment());
+
+  const today = getMoment; // today == moment() 입니다.
+  const firstWeek = today.clone().startOf('month').week();
+  const lastWeek =
+    today.clone().endOf('month').week() === 1
+      ? 53
+      : today.clone().endOf('month').week();
+
+  const calendarArr = (): JSX.Element[] => {
+    let result: JSX.Element[] = [];
+    let week = firstWeek;
+    for (week; week <= lastWeek; week++) {
+      result = result.concat(
+        <tr key={week}>
+          {Array(7)
+            .fill(0)
+            .map((data, index) => {
+              let days = today
+                .clone()
+                .startOf('year')
+                .week(week)
+                .startOf('week')
+                .add(index, 'day');
+
+              if (moment().format('YYYYMMDD') === days.format('YYYYMMDD')) {
+                return (
+                  <td key={index} style={{ backgroundColor: 'red' }}>
+                    <span>{days.format('D')}</span>
+                  </td>
+                );
+              } else if (days.format('MM') !== today.format('MM')) {
+                return (
+                  <td key={index} style={{ backgroundColor: 'gray' }}>
+                    <span>{days.format('D')}</span>
+                  </td>
+                );
+              } else {
+                return (
+                  <td key={index}>
+                    <span>{days.format('D')}</span>
+                  </td>
+                );
+              }
+            })}
+        </tr>
+      );
+    }
+    return result;
+  };
+
+  return (
+    <div className={styles.calendar}>
+      <div className="control">
+        <button
+          onClick={() => {
+            setMoment(getMoment.clone().subtract(1, 'month'));
+          }}
+        >
+          이전달
+        </button>
+        <span>{today.format('YYYY 년 MM 월')}</span>
+        {/* YYYY는 년도 MM은 달 입니다. */}
+        <button
+          onClick={() => {
+            setMoment(getMoment.clone().add(1, 'month'));
+          }}
+        >
+          다음달
+        </button>
+      </div>
+      <table>
+        <tbody>{calendarArr()}</tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Calendar;

de43c18468ee3609b833f729405f9ef9c00d30e6
TMInstaller <ajvldmlqjqclr@naver.com>
2023-04-01 19:24:04 +0900

    feat: BannerImage 메인페이지에 띄우기 추가

--- src/components/banner/BannerMainLayout.tsx
+++ src/components/banner/BannerMainLayout.tsx
@@ -1,7 +1,17 @@
+import Image from 'next/image';
 import styles from '../../styles/components/banner/BannerMainLayout.module.scss';
+import BannerImage from '../../../public/temp_images/BannerTempImage.png';
 
 const BannerMainLayout = () => {
-  return <div className={styles.bannerMainLayout}>Banner입니다</div>;
+  return (
+    <div className={styles.BannerMainLayout}>
+      <Image
+        src={BannerImage}
+        alt="banner image"
+        className={styles.BannerImage}
+      />
+    </div>
+  );
 };
 
 export default BannerMainLayout;

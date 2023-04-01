e9a94102d8d48dd584dee487d78ca5a94cc28ff1
TMInstaller <ajvldmlqjqclr@naver.com>
2023-04-01 18:33:23 +0900

    feat: .sass 파일 적용, 스크롤 테스트를 위한 더미 코드 추가

--- src/components/routine/RoutineLayout.tsx
+++ src/components/routine/RoutineLayout.tsx
@@ -1,9 +1,27 @@
+import styles from '../../styles/components/routine/RoutineLayout.module.scss';
+
 const RoutineLayout = () => {
   return (
-    <div className="RoutineLayout">
-      <div className="RoutineLayoutTop">ROUTINE</div>
-      <div className="RoutineLayoutBottom">
-        Routine1 <br /> Routine2
+    <div className={styles.RoutineLayout}>
+      <div className={styles.RoutineLayoutTop}>ROUTINE</div>
+      <div className={styles.RoutineLayoutBottom}>
+        Routine1 <br /> Routine2 <br /> Routine 3 <br />
+        Routine1 <br /> Routine2 <br /> Routine 3 <br />
+        Routine1 <br /> Routine2 <br /> Routine 3 <br />
+        Routine1 <br /> Routine2 <br /> Routine 3 <br />
+        Routine1 <br /> Routine2 <br /> Routine 3 <br />
+        Routine1 <br /> Routine2 <br /> Routine 3 <br />
+        Routine1 <br /> Routine2 <br /> Routine 3 <br />
+        Routine1 <br /> Routine2 <br /> Routine 3 <br />
+        Routine1 <br /> Routine2 <br /> Routine 3 <br />
+        Routine1 <br /> Routine2 <br /> Routine 3 <br />
+        Routine1 <br /> Routine2 <br /> Routine 3 <br />
+        Routine1 <br /> Routine2 <br /> Routine 3 <br />
+        Routine1 <br /> Routine2 <br /> Routine 3 <br />
+        Routine1 <br /> Routine2 <br /> Routine 3 <br />
+        Routine1 <br /> Routine2 <br /> Routine 3 <br />
+        Routine1 <br /> Routine2 <br /> Routine 3 <br />
+        Routine1 <br /> Routine2 <br /> Routine 3 <br />
       </div>
     </div>
   );

c3261e867c6cd61ad41a2a2b4786c9ef9e57e245
TMInstaller <ajvldmlqjqclr@naver.com>
2023-04-01 20:16:13 +0900

    feat: BannerMainLayout POC 기능 완료

--- src/components/banner/BannerMainLayout.tsx
+++ src/components/banner/BannerMainLayout.tsx
@@ -1,15 +1,84 @@
-import Image from 'next/image';
+import { useState, useEffect } from 'react';
+import Image, { StaticImageData } from 'next/image';
 import styles from '../../styles/components/banner/BannerMainLayout.module.scss';
-import BannerImage from '../../../public/temp_images/BannerTempImage.png';
+import BannerImage1 from '../../../public/temp_images/BannerTempImage1.png';
+import BannerImage2 from '../../../public/temp_images/BannerTempImage2.png';
+import BannerImage3 from '../../../public/temp_images/BannerTempImage3.png';
+import BannerImage4 from '../../../public/temp_images/BannerTempImage4.png';
+import BannerImage5 from '../../../public/temp_images/BannerTempImage5.png';
 
-const BannerMainLayout = () => {
+const IMAGE_URLS: StaticImageData[] = [
+  BannerImage1,
+  BannerImage2,
+  BannerImage3,
+  BannerImage4,
+  BannerImage5,
+];
+
+interface BannerMainLayoutProps {
+  imageUrls?: StaticImageData[];
+}
+
+const BannerMainLayout: React.FC<BannerMainLayoutProps> = ({
+  imageUrls = IMAGE_URLS,
+}) => {
+  const [currentImageIndex, setCurrentImageIndex] = useState<number>(0);
+
+  const onNextButtonClick = () => {
+    setCurrentImageIndex((currentIndex: number) =>
+      currentIndex === IMAGE_URLS.length - 1 ? 0 : currentIndex + 1
+    );
+  };
+
+  const onPrevButtonClick = () => {
+    setCurrentImageIndex((currentIndex: number) =>
+      currentIndex === 0 ? IMAGE_URLS.length - 1 : currentIndex - 1
+    );
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      onNextButtonClick();
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className={styles.BannerMainLayout}>
-      <Image
-        src={BannerImage}
-        alt="banner image"
-        className={styles.BannerImage}
-      />
+      <div className={styles.BannerImageWrapper}>
+        <Image
+          src={IMAGE_URLS[currentImageIndex]}
+          alt="banner image"
+          className={styles.BannerImage}
+          priority={true} // 가장 용량이 큰 이미지부터 로드하기 위한 속성
+        />
+        <div className={styles.BannerImageOverlay}>
+          <div className={styles.ArrowButtonWrapper}>
+            <button
+              className={styles.PrevButton}
+              onClick={onPrevButtonClick}
+              aria-label="previous image"
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <path d="M15 3l-9 9 9 9" />
+              </svg>
+            </button>
+            <div className={styles.ImageIndex}>
+              {currentImageIndex + 1}/{IMAGE_URLS.length}
+            </div>
+            <button
+              className={styles.NextButton}
+              onClick={onNextButtonClick}
+              aria-label="next image"
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <path d="M9 21l9-9-9-9" />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };

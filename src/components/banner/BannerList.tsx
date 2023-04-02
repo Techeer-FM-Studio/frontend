0f62b9b24751765c6d20e84f76620cd9e4611855
jonghun <minn4072@gmail.com>
2023-04-02 23:17:51 +0900

    feat: 배너 리스트 컴포넌트 구현

--- src/components/banner/BannerList.tsx
+++ src/components/banner/BannerList.tsx
@@ -1,25 +1,18 @@
 import React from 'react';
 import BannerItem from './BannerItem';
+import styles from '../../styles/components/banner/BannerList.module.scss';
+import { BannerItemType } from '@/types/banner';
 
-function BannerList() {
+function BannerList({
+  bannerList,
+}: {
+  bannerList: BannerItemType[] | undefined;
+}) {
   return (
-    <div>
-      <BannerItem
-        item={{
-          url: 'https://i.pinimg.com/564x/62/cd/fa/62cdfaba81ec4a1035af0476bb3d57b5.jpg',
-          like: 29,
-          veiws: 40,
-          writer: '전종훈',
-        }}
-      ></BannerItem>
-      <BannerItem
-        item={{
-          url: 'https://i.pinimg.com/564x/62/cd/fa/62cdfaba81ec4a1035af0476bb3d57b5.jpg',
-          like: 29,
-          veiws: 40,
-          writer: '전종훈',
-        }}
-      ></BannerItem>
+    <div className={styles.container}>
+      {bannerList?.map((item: BannerItemType) => (
+        <BannerItem item={item}></BannerItem>
+      ))}
     </div>
   );
 }

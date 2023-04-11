591e827213aea1f1d16aaeea0301ea4b03caf594
TMInstaller <ajvldmlqjqclr@naver.com>
2023-04-11 18:13:57 +0900

    comment: 필요한 주석 추가

--- src/utils/getTimeLeftDisplay.ts
+++ src/utils/getTimeLeftDisplay.ts
@@ -3,15 +3,17 @@ const getTimeLeftDisplay = (timeLeft: {
   hours: number;
   minutes: number;
 }) => {
-  let display = '';
+  let display = ''; // 빈 문자열 생성
   if (timeLeft.days > 0) {
-    display += `${timeLeft.days}일 `;
+    // 남은 일 수가 0보다 큰 경우
+    display += `${timeLeft.days}일 `; // display 문자열에 `${timeLeft.days}일 ` 추가
   }
   if (timeLeft.hours > 0) {
-    display += `${timeLeft.hours}시간 `;
+    // 남은 시간이 0보다 큰 경우
+    display += `${timeLeft.hours}시간 `; // display 문자열에 `${timeLeft.hours}시간 ` 추가
   }
-  display += `${timeLeft.minutes}분`;
-  return display;
+  display += `${timeLeft.minutes}분`; // display 문자열에 `${timeLeft.minutes}분` 추가
+  return display; // display 문자열 반환
 };
 
-export default getTimeLeftDisplay;
+export default getTimeLeftDisplay; // getTimeLeftDisplay 함수를 모듈의 기본 내보내기로 설정

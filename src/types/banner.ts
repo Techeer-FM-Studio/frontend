5adbe9ead648ca25683434a1f08778e381059923
jonghun <minn4072@gmail.com>
2023-04-14 02:10:51 +0900

    feat: 배너 타입 정의

--- src/types/banner.ts
+++ src/types/banner.ts
@@ -1,8 +1,24 @@
 // TODO: api 설계서 나오면 수정하기
 export type BannerItemType = {
   id: number;
-  url: string; //s3 주소
-  like: number; //좋아요
-  veiws: number; //조회수
-  writer: string; //작성자
+  type: string;
+  owner: string;
+  title: string;
+  memo: string;
+  startAt: string;
+  endAt: string;
+  imageUrl: string[];
+  likeCnt: number;
+  finished: boolean;
+  isLiked: boolean;
+  isIncluded: boolean;
+  readCnt: number;
+};
+
+export type BannerPageableType = {
+  totalPages: number;
+  totalElements: number;
+  size: number;
+  page: number;
+  content: BannerItemType[];
 };

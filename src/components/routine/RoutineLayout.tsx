042b08a9a6b3e3911b745b1c67f4e3299115edf8
TMInstaller <ajvldmlqjqclr@naver.com>
2023-04-15 15:48:02 +0900

    feat: 디자인 변경, 일정 등록 버튼 추가

--- src/components/routine/RoutineLayout.tsx
+++ src/components/routine/RoutineLayout.tsx
@@ -10,15 +10,16 @@ const RoutineLayout: React.FC<RoutineLayoutProps> = ({ showForm }) => {
       <div className={styles.RoutineLayoutTop}>ROUTINE</div>
       <div className={styles.RoutineLayoutBottom}>
         {showForm && (
-          <div>
+          <div className={styles.RoutineAddForm}>
             <h3>제목</h3>
-            <input type="text" />
+            <input type="text" className={styles.input} />
             <h3>시작 날짜</h3>
-            <input type="date" />
+            <input type="date" className={styles.input} />
             <h3>종료 날짜</h3>
-            <input type="date" />
+            <input type="date" className={styles.input} />
             <h3>메모</h3>
-            <textarea />
+            <textarea className={styles.textarea} />
+            <button className={styles.button}>일정 등록하기</button>
           </div>
         )}
       </div>

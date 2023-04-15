526a306ed6fc0b5b4913ea8a9abf5c705a8fc2c8
TMInstaller <ajvldmlqjqclr@naver.com>
2023-04-15 15:10:31 +0900

    feat: 디자인 및 날짜 출력 형식 변경

--- src/components/routine/RoutineLayout.tsx
+++ src/components/routine/RoutineLayout.tsx
@@ -1,5 +1,6 @@
 import { TaskInfo } from '@/types/routine';
 import styles from '../../styles/components/routine/RoutineLayout.module.scss';
+import moment from 'moment';
 
 interface RoutineLayoutProps {
   selectedTasks: TaskInfo[];
@@ -11,11 +12,26 @@ const RoutineLayout = ({ selectedTasks }: RoutineLayoutProps) => {
       <div className={styles.RoutineLayoutTop}>ROUTINE</div>
       <div className={styles.RoutineLayoutBottom}>
         {selectedTasks?.length > 0 ? (
-          selectedTasks.map((task, index) => (
-            <div key={task.taskId || index} className={styles.TaskItem}>
-              {task.title}
-            </div>
-          ))
+          selectedTasks.map((task, index) => {
+            const startAt = moment(task.startAt).format(
+              'YYYY년 MM월 DD일 HH시 mm분'
+            );
+            const endAt = moment(task.endAt).format(
+              'YYYY년 MM월 DD일 HH시 mm분'
+            );
+
+            return (
+              <div key={task.taskId || index} className={styles.TaskItem}>
+                <div className={styles.TaskItemTitle}>제목: {task.title}</div>
+                <div className={styles.TaskItemDetails}>
+                  <div>writer: {task.writer}</div>
+                  <div>memo: {task.memo}</div>
+                  <div>시작: {startAt}</div>
+                  <div>종료: {endAt}</div>
+                </div>
+              </div>
+            );
+          })
         ) : (
           <div>No tasks for the selected day</div>
         )}

1ac2cd39c576e6bc06cce688df8f63fed807b72f
jonghun <minn4072@gmail.com>
2023-04-17 05:47:50 +0900

    feat: 좋아요/달력 버튼 컴포넌트 구현 및 기능 추가

--- src/components/banner/detail/DetailButtons.tsx
+++ src/components/banner/detail/DetailButtons.tsx
@@ -1,9 +1,39 @@
-function DetailButtons() {
+import { postCalender } from '@/apis/postCalender';
+import { postLike } from '@/apis/postLike';
+import { useState } from 'react';
+
+function DetailButtons({
+  id,
+  likeCnt,
+  isLiked,
+  isIncluded,
+}: {
+  id: number;
+  likeCnt: number;
+  isLiked: boolean;
+  isIncluded: boolean;
+}) {
+  const [like, setLike] = useState(isLiked);
+  const [include, setInclude] = useState(isIncluded);
+
+  const onChangeLike = async () => {
+    const data = await postLike('test1', id);
+    setLike(data.isLiked);
+  };
+
+  const onChangeCalender = async () => {
+    const data = await postCalender('test1', id);
+    setInclude(data.isIncluded);
+  };
+
   return (
     <section>
-      <button>좋아요 : 300</button>
-      <button>내 달력에 추가하기</button>
-      <button>댓글 : 23</button>
+      <button onClick={onChangeLike} style={like ? { background: 'pink' } : {}}>
+        좋아요 : {likeCnt}
+      </button>
+      <button onClick={onChangeCalender}>
+        {include ? '이미 달력에 추가되었습니다' : '내 달력에 추가하기'}
+      </button>
     </section>
   );
 }

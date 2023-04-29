fefd38ed128ca4538890a5bc7412f2eb700d90c4
jonghun <minn4072@gmail.com>
2023-04-29 18:09:49 +0900

    refactor: 댓글 리스트 컴포넌트 수정

--- src/components/banner/detail/CommentList.tsx
+++ src/components/banner/detail/CommentList.tsx
@@ -1,36 +1,40 @@
-import { BannerComment } from '@/types/banner';
-import React, { useState } from 'react';
+import { BannerCommentPageableType } from '@/types/banner';
+import React, { useEffect, useState } from 'react';
 import CommentItem from './CommentItem';
 import { postComment } from '@/apis/postComment';
+import { getComments } from '@/apis/getComments';
+import styles from '../../../styles/components/banner/detail/BannerCommetList.module.scss';
 
-function CommentList({
-  id,
-  commentList,
-}: {
-  id: number;
-  commentList: BannerComment[] | undefined;
-}) {
+function CommentList({ id }: { id: number }) {
   const [text, setText] = useState('');
-  const onChangeText = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const [commentList, setCommentList] = useState<BannerCommentPageableType>();
+  const onChangeText = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setText(e.currentTarget.value);
   };
 
   const onSubmitComment = () => {
-    postComment(text, 'test2', id);
+    postComment(text, 'test1', id);
     setText('');
   };
 
+  useEffect(() => {
+    getComments(id, 0, 6).then((res) => setCommentList({ ...res }));
+  }, [id]);
+
   return (
-    <section>
-      <input
-        type="text"
-        placeholder="댓글 입력해주세요."
-        value={text}
-        onChange={onChangeText}
-      />
-      <button onClick={onSubmitComment}>댓글 달기</button>
-      {commentList?.map((comment) => (
-        <CommentItem key={comment.commentId} comment={comment} />
+    <section className={styles.container}>
+      <article>
+        <div>{commentList?.content.length}개의 댓글</div>
+        <textarea
+          placeholder="댓글 입력해주세요."
+          value={text}
+          onChange={onChangeText}
+        />
+        <button onClick={onSubmitComment}>댓글 달기</button>
+      </article>
+
+      {commentList?.content.map((comment) => (
+        <CommentItem key={comment.id} bannerId={id} commentData={comment} />
       ))}
     </section>
   );

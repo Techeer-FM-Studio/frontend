55bbb9e96ea43d2d42b7fba6b38e4a05ea540db5
jonghun <minn4072@gmail.com>
2023-04-29 18:12:52 +0900

    feat: next api mock data 설정

---
+++ src/pages/api/v1/banners/custom/[id]/comments/page/index.ts
@@ -0,0 +1,31 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Object>
+) {
+  const mockData = {
+    totalPages: 5,
+    totalElements: 10,
+    size: 2,
+    page: 3,
+    content: [
+      {
+        id: 1,
+        writer: 'test1',
+        comments: 'test memo',
+        createdAt: '2023-04-07T02:53:10.682818',
+        updatedAt: '2023-04-07T02:53:10.682818',
+      },
+      {
+        id: 2,
+        writer: 'test2',
+        comments: '<p>dsafdsafd</p>',
+        createdAt: '2023-04-07T02:53:10.682818',
+        updatedAt: '2023-04-07T02:53:10.682818',
+      },
+    ],
+  };
+
+  return res.status(200).json(mockData);
+}

0fa36090664c4a02d4802b67531dde430629d402
jonghun <minn4072@gmail.com>
2023-04-29 18:08:47 +0900

    refactor: 이미지 보내기 api 수정

--- src/apis/postImages.ts
+++ src/apis/postImages.ts
@@ -1,10 +1,18 @@
 import { axiosCustom } from './createAxios';
 
 export async function postImages(files: FileList | any) {
+  if (files === undefined)
+    return {
+      imageUrl:
+        'https://i.pinimg.com/564x/92/6a/44/926a44c38c195bdabb29bea6bbdc1724.jpg',
+    };
+
   const formData = new FormData();
 
   Array.from(files).forEach((el: any) => {
-    formData.append('userfile', el);
+    // 노션에 나와있는 키값
+    console.log('files', el);
+    formData.append('image', el);
   });
 
   for (const value of formData) {
@@ -12,8 +20,11 @@ export async function postImages(files: FileList | any) {
   }
 
   return await axiosCustom
-    .post('images', formData, {
+    .post('banners/images', formData, {
       headers: { 'Content-Type': 'multipart/form-data' },
     })
-    .then((res) => res.data);
+    .then((res) => {
+      console.log(res.data);
+      return res.data;
+    });
 }

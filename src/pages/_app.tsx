27a033420e98abcaea5a21cf34b926df8439d975
jonghun <minn4072@gmail.com>
2023-05-10 23:59:22 +0900

    tt

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
-import '@/styles/globals.scss';
-import type { AppProps } from 'next/app';
-import Header from '@/components/common/Header';
+import '@/styles/globals.scss'
+import type { AppProps } from 'next/app'
+import Header from '@/components/common/Header'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Header />
-      <Component {...pageProps} />;
-    </>
-  );
+    console.log('hello')
+    var lo = 10;
+    return (
+        <>
+            <Header />
+            <Component {...pageProps} />;
+        </>
+    )
 }

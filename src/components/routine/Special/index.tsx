2094051538dfa1645967ef557f2deba943529356
TMInstaller <ajvldmlqjqclr@naver.com>
2023-06-01 16:23:41 +0900

    refact: BEM 방법론 적용

--- src/components/routine/Special/index.tsx
+++ src/components/routine/Special/index.tsx
@@ -77,10 +77,10 @@ const RoutineSpecial = () => {
   };
 
   return (
-    <div className={styles.NextRoutineLayout}>
-      <div className={styles.SelectMenu}>NextRoutine</div>
+    <div className={styles.routineSpecial}>
+      <div className={styles.routineSpecial__selectMenu}>NextRoutine</div>
       {nextRoutineData ? (
-        <div className={styles.RoutineInfo}>
+        <div className={styles.routineSpecial__info}>
           <p>Writer: {nextRoutineData?.writer}</p>
           <p>Title: {nextRoutineData?.title}</p>
           <p>Memo: {nextRoutineData?.memo}</p> <br />
@@ -91,7 +91,9 @@ const RoutineSpecial = () => {
           <p>
             End Time: {new Date(nextRoutineData?.endAt).toLocaleString('ko-KR')}
           </p>
-          <p>Time Left: {timeLeftDisplay()}</p>
+          <p className={styles.routineSpecial__timeLeft}>
+            Time Left: {timeLeftDisplay()}
+          </p>
           <p>Status: {routineStatus}</p>
         </div>
       ) : (

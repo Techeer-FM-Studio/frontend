44d76d79fbc732099bd74dde6f7ef0bea6ffb90b
jonghun <minn4072@gmail.com>
2023-06-28 23:59:09 +0900

    feat: style test

--- src/components/Sign/SignUp/index.tsx
+++ src/components/Sign/SignUp/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-
+import styles from './styles.module.scss';
 function SignUp() {
-  return <div>회원가입 컴포넌트</div>;
+  return <div className={styles.test}>회원가입 컴포넌트</div>;
 }
 
 export default SignUp;
